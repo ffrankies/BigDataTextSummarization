301af660a4b7800b40b599ef09697546b9dd1920
Matt Tuckman <matt4@vt.edu>
2018-09-12 16:06:37 -0400

    Mid point into synsets

--- unit1/WordFreq.py
+++ unit1/WordFreq.py
@@ -10,6 +10,7 @@ from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from nltk.corpus import wordnet
 
+
 def parse_arguments():
     """Parses command-line arguments.
     Returns:
@@ -19,6 +20,8 @@ def parse_arguments():
     parser.add_argument('-f', '--file', type=str, help='The path to the JSON file containing processed text.')
     parser.add_argument('-n', '--number', type=int, help='The numbers of words that this program will output.')
     return parser.parse_args()
+
+
 # End of parse_arguments()
 
 
@@ -34,6 +37,8 @@ def load_records(file):
     records = [json.loads(record) for record in records]
     records = list(filter(lambda record: record[constants.TEXT] != '', records))
     return records
+
+
 # End of load_records()
 
 
@@ -49,7 +54,7 @@ def preprocess_freq(jsonarr):
     sentences = map(lambda data: data[constants.TEXT].lower(), jsonarr)
     # punctuation and stopwords to remove
     stoplist = stopwords.words('english') + list(string.punctuation) \
-        + constants.CONTRACTIONS + constants.MYSQL_STOPWORDS
+               + constants.CONTRACTIONS + constants.MYSQL_STOPWORDS
 
     lemmatizer = WordNetLemmatizer()
     stemmer = SnowballStemmer('english')
@@ -61,19 +66,57 @@ def preprocess_freq(jsonarr):
         # filter list of words to remove stop words and punctuation
         filtered = list(filter(lambda word: word not in stoplist, words))
         # get stem of all words
-        stemmed = map(lambda word: stemmer.stem(word), filtered)
+        # TODO stemmed = map(lambda word: stemmer.stem(word), filtered)
         # lemmatize all words
-        lemmatized = map(lambda word: lemmatizer.lemmatize(word), stemmed)
+        # TODO lemmatized = map(lambda word: lemmatizer.lemmatize(word), stemmed)
 
+        # TODO remove
+        lemmatized = map(lambda word: lemmatizer.lemmatize(word), filtered)
         cleaned.extend(lemmatized)
 
     return cleaned
+
+
 # End of preprocess_freq
 
+def generate_syn_set(freq_list):
+    freq_hash = set(freq_list)
+    general_synonyms = {}
+    relevant_synonyms = {}
+
+    # Get the synonyms and their hypernyms for each of the top 10 most frequent works and store the word mapped to them
+    for i in range(10):
+        curr_general_synonyms = wordnet.synsets(freq_list[i])
+        extended_synonyms = []
+        for curr_syn in curr_general_synonyms:
+            extended_synonyms.extend(curr_syn.hypernyms())
+            extended_synonyms.append(curr_syn)
+
+        extended_synonyms = list(map(lambda x: x.name().split('.')[0], extended_synonyms))
+        print(freq_list[i], extended_synonyms)
+        general_synonyms.update({freq_list[i]: extended_synonyms})
+
+
+    # Store 10 most frequent works in relevant_synonyms mapped to an empty array
+    for j in range(10):
+        relevant_synonyms.update({freq_list[j]: []})
+
+    for word in general_synonyms:
+        curr_syns = general_synonyms[word]
+        for syn in curr_syns:
+            if syn != word and syn in freq_hash and syn not in relevant_synonyms[word]:
+                relevant_synonyms[word].append(syn)
+
+    return relevant_synonyms
+
+
+# End of generate_syn_set
 
 def get_freq(wordlist, count=10):
     freqDist = FreqDist(wordlist)
     return freqDist.most_common(count)
+
+
 # End of get_freq
 
 
@@ -84,15 +127,13 @@ if __name__ == "__main__":
 
     jsonArr = load_records(jsonFilePath)
     processed = preprocess_freq(jsonArr)
-    freq_words = get_freq(processed, num)
-    no_freq_words = list(map(lambda tup: tup[0], freq_words))
 
-    syns = {}
-    for word in no_freq_words:
-        syns.update({word: wordnet.synsets(word)})
+    freq_words = get_freq(processed, 1000)
+    no_count_freq_words = list(map(lambda tup: tup[0], freq_words))
 
-    print(freq_words, '\n')
+    # TODO print(freq_words, '\n')
+
+    syns = generate_syn_set(no_count_freq_words)
 
     for s in syns:
         print('%10s: ' % s, syns[s])
-
