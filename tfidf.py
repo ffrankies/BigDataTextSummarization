52e2f22a79d4a958becf001aa7a1744c883d727c
Frank Wanye <wanyef@mail.gvsu.edu>
2018-09-18 12:05:50 -0400

    tf-idf algorithm working

--- tfidf.py
+++ tfidf.py
@@ -6,6 +6,7 @@ import math
 from nltk.probability import FreqDist
 
 import wordcount
+import constants
 
 
 def term_frequency(tokenized_records):
@@ -31,20 +32,17 @@ def term_frequency(tokenized_records):
 # End of term_frequency()
 
 
-def inverse_document_frequency(records, unique_words):
+def inverse_document_frequency(lemmatized_records, unique_words):
     """Calculates the inverse document frequency of the tokenized lemmas in the records. Inverse document frequency
     is calculated as log (number of records in which word appears / total number of records)
 
     Params:
-    - records (list): The records loaded from the JSON file
+    - lemmatized_records (list): The lemmatized versions of the records loaded from the JSON file
     - unique_words (list): The list of unique words in the records
 
     Returns:
     - inverse_document_frequencies (dict): The inverse document frequency of each word
     """
-    contents = map(lambda record: record[constants.TEXT], records)
-    tokenized_records = [wordcount.word_tokenize(record.lower()) for record in contents]
-    lemmatized_records = wordcount.lemmatize_words(tokenized_records)
     inverse_document_frequencies = dict()
     num_records = len(lemmatized_records)
     for word in unique_words:
@@ -58,8 +56,50 @@ def inverse_document_frequency(records, unique_words):
 # End of inverse_document_frequency()
 
 
+def tf_idf(records):
+    """Calculates the term frequency inverse document frequency for each lemmatized non-stopword in the dataset of
+    records.
+
+    Params:
+    - records (list): The records loaded from the dataset
+
+    Returns:
+    - tf_idf_scores (list): The tf-idf score for each lemmatized non-stopword in the dataset
+    """
+    contents = map(lambda record: record[constants.TEXT], records)
+    word_tokenized_records = [wordcount.word_tokenize(record.lower()) for record in contents]
+    lemmatized_records = wordcount.lemmatize_words(word_tokenized_records)
+    tokenized_records = list()
+    for lemmatized_record in lemmatized_records:
+        tokenized_records.extend(lemmatized_record)
+    term_frequencies = term_frequency(tokenized_records)
+    inverse_document_frequencies = inverse_document_frequency(lemmatized_records, term_frequencies.keys())
+    tf_idf_scores = list()
+    for word in term_frequencies.keys():
+        tf_idf_scores.append((word, term_frequencies[word] * inverse_document_frequencies[word]))
+    return tf_idf_scores
+# End of tf_idf()
+
+
+def extract_important_words(tf_idf_scores, num_words):
+    """Extracts important words based on their tf_idf_scores.
+
+    Params:
+    - tf_idf_scores (list): The tf-idf score of each word in the document
+    - num_words (int): The number of words to extract
+
+    Returns:
+    - important_words (list): The list of words with the highest tf-idf score
+    """
+    sorted_scores = sorted(tf_idf_scores, key=lambda score: score[1])
+    important_words = [score[0] for score in sorted_scores[:num_words]]
+    return important_words
+# End of extract_important_words()
+
+
 if __name__ == "__main__":
     args = wordcount.parse_arguments()
     records = wordcount.load_records(args.file, False)
-    tokenized_records = wordcount.tokenize_records(records)
-    term_frequency(tokenized_records)
+    tf_idf_scores = tf_idf(records)
+    important_words = extract_important_words(tf_idf_scores, args.num_words)
+    print(important_words)
